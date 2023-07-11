24f517c934525f670dff29950c524e77b43f66f8
babultalukdar <talukdar.mca@gmail.com>
2023-07-11 14:33:16 +0530

    commit delete

--- projectAngular/src/app/blue-background.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BlueBackgroundDirective } from './blue-background.directive';
-
-describe('BlueBackgroundDirective', () => {
-  it('should create an instance', () => {
-    const directive = new BlueBackgroundDirective();
-    expect(directive).toBeTruthy();
-  });
-});
